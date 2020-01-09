ce287a1eb21109dad66883771b08ef69a73462d0
Enricco Diniz <enriccoamaral@hotmail.com>
2020-01-09 11:28:55 -0300

    Initial comit
    Adding configuration for the lint , sequelize, nodemon and sequelize

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins:[
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier":"error",
+    "no-param-reassign":"off",
+    "class-method-use-this":"off",
+    "no-unused-vars":['error',{"argsIgnorePattern":"next"}],
+    "camelcase":"off"
+  },
+};

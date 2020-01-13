b3b03137313dc2fb2bf438c7d02e1ee1bc2a5011
Enricco Diniz <enriccoamaral@hotmail.com>
2020-01-13 14:25:53 -0300

    Upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,29 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
+import multerConfig from './config/multer';
+
+import authMiddleware from './app/middlewares/auth';
 
 const router = new Router();
+const upload = multer(multerConfig);
 
 router.post('/users', (req, res) => UserController.store(req, res));
 
 router.post('/sessions', (req, res) => SessionController.store(req, res));
+
+router.use(authMiddleware);
+
+router.put('/users', (req, res) => UserController.update(req, res));
+
+router.post('/files', upload.single('file'), (req, res) =>
+  FileController.store(req, res)
+);
+
 router.get('/', (req, res) => {
   return res.json({ message: 'oi' });
 });

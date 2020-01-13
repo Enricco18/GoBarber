4133a5559b4a094ee13fd790a7b10ef8f56213cf
Enricco Diniz <enriccoamaral@hotmail.com>
2020-01-13 10:09:06 -0300

    SessionController criado
    Criar a sessão habilitado

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const router = new Router();
 
 router.post('/users', (req, res) => UserController.store(req, res));
 
+router.post('/sessions', (req, res) => SessionController.store(req, res));
 router.get('/', (req, res) => {
   return res.json({ message: 'oi' });
 });
